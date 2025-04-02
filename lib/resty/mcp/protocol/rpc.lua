e2fa3fd23e0c4841bf84bdbb7ce92c15cf4e7819
RangerUFO <ufownl@gmail.com>
2025-04-02 17:47:50 +0800

    Initial commit
    Implement JSONRPC protocol packing

---
+++ lib/resty/mcp/protocol/rpc.lua
@@ -0,0 +1,81 @@
+local _M = {
+  _NAME = "resty.mcp.protocol.rpc",
+  _VERSION = "1.0"
+}
+
+local mcp = {
+  utils = require("resty.mcp.utils")
+}
+
+local cjson = require("cjson.safe")
+
+function _M.request(method, params)
+  if type(method) ~= "string" then
+    error("JSONRPC: method MUST be a string.")
+  end
+  if params and (type(params) ~= "table" or #params > 0) then
+    error("JSONRPC: params MUST be a dict.")
+  end
+  local rid = mcp.utils.generate_id()
+  local body, err = cjson.encode({
+    jsonrpc = "2.0",
+    id = rid,
+    method = method,
+    params = params or nil
+  })
+  return body, rid, err
+end
+
+function _M.succ_resp(rid, result)
+  if type(rid) ~= "string" and (type(rid) ~= "number" or rid % 1 ~= 0) then
+    error("JSONRPC: ID MUST be a string or integer.")
+  end
+  if type(result) ~= "table" or #result > 0 then
+    error("JSONRPC: result MUST be a dict.")
+  end
+  local body, err = cjson.encode({
+    jsonrpc = "2.0",
+    id = rid,
+    result = result
+  })
+  return body, err
+end
+
+function _M.fail_resp(rid, code, message, data)
+  if type(rid) ~= "string" and (type(rid) ~= "number" or rid % 1 ~= 0) then
+    error("JSONRPC: ID MUST be a string or integer.")
+  end
+  if type(code) ~= "number" or code % 1 ~= 0 then
+    error("JSONRPC: error code MUST be an integer.")
+  end
+  if type(message) ~= "string" then
+    error("JSONRPC: error message MUST be a string.")
+  end
+  local body, err = cjson.encode({
+    jsonrpc = "2.0",
+    id = rid,
+    error = {
+      code = code,
+      message = message,
+      data = data
+    }
+  })
+  return body, err
+end
+
+function _M.notification(method, params)
+  if type(method) ~= "string" then
+    error("JSONRPC: method MUST be a string.")
+  end
+  if params and (type(params) ~= "table" or #params > 0) then
+    error("JSONRPC: params MUST be a dict.")
+  end
+  local body, err = cjson.encode({
+    jsonrpc = "2.0",
+    method = method,
+    params = params or nil
+  })
+  return body, err
+end
+
+return _M
---
+++ lib/resty/mcp/utils.lua
@@ -0,0 +1,24 @@
+local _M = {
+  _NAME = "resty.mcp.utils",
+  _VERSION = "1.0"
+}
+
+local resty_random = require("resty.random")
+local resty_sha256 = require("resty.sha256")
+local ngx_base64 = require("ngx.base64")
+local ngx_worker_pid = ngx.worker.pid
+local ngx_time = ngx.time
+
+local id_counter = 0
+
+function _M.generate_id()
+  local sha256 = resty_sha256.new()
+  sha256:update(ngx_worker_pid().."&")
+  sha256:update(ngx_time().."&")
+  sha256:update(id_counter.."&")
+  id_counter = (id_counter + 1) % 1000
+  sha256:update(resty_random.bytes(8))
+  return ngx_base64.encode_base64url(sha256:final())
+end
+
+return _M
