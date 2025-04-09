3b4d74a003fa21a1953871379f672a4a2e8caf8f
RangerUFO <ufownl@gmail.com>
2025-04-09 14:53:59 +0800

    Implement sync request mode for blocking IO transport

--- lib/resty/mcp/session.lua
+++ lib/resty/mcp/session.lua
@@ -13,6 +13,133 @@ local ngx_thread_kill = ngx.thread.kill
 local ngx_thread_wait = ngx.thread.wait
 local unpack = table.unpack or unpack
 
+local function handle_message(self, msg)
+  local reply = mcp.rpc.handle(msg, self.methods, function(rid, result, errobj)
+    local cb = self.pending_requests[rid]
+    if not cb then
+      ngx_log(ngx.ERR, "response: request id mismatch")
+      return
+    end
+    self.pending_requests[rid] = nil
+    cb(result, errobj)
+  end)
+  if reply then
+    local ok, err = self.conn:send(reply)
+    if not ok then
+      ngx_log(ngx.ERR, "transport: ", err)
+      return nil, err
+    end
+  end
+  return true
+end
+
+local function main_loop(self)
+  local running_tasks = self.bg_tasks and {} or nil
+  while true do
+    local msg, err = self.conn:recv()
+    if msg then
+      if running_tasks then
+        local tid = mcp.utils.generate_id()
+        running_tasks[tid] = ngx_thread_spawn(function()
+          self.bg_tasks.count = self.bg_tasks.count + 1
+          handle_message(self, msg)
+          self.bg_tasks.count = self.bg_tasks.count - 1
+          running_tasks[tid] = nil
+          if self.bg_tasks.count < 1 and self.bg_tasks.waiting > 0 then
+            self.bg_tasks.sema:post(self.bg_tasks.waiting)
+          end
+        end)
+      else
+        local ok, err = handle_message(self, msg)
+        if not ok then
+          break
+        end
+      end
+    elseif err ~= "timeout" then
+      break
+    end
+  end
+  if running_tasks then
+    local tasks = {}
+    for k, v in pairs(running_tasks) do
+      table.insert(tasks, v)
+    end
+    if #tasks > 0 then
+      local ok, err = ngx_thread_wait(unpack(tasks))
+      if not ok then
+        ngx_log(ngx.ERR, "ngx thread: ", err)
+      end
+    end
+  end
+  self.main_loop = nil
+end
+
+local function request_async(self, req, rid, cb)
+  local ok, err = self.conn:send(req)
+  if not ok then
+    return nil, err
+  end
+  self.pending_requests[rid] = cb
+  return true
+end
+
+local function return_result(result, errobj)
+  if errobj then
+    if errobj.data then
+      local data, err = cjson.encode(errobj.data)
+      return nil, string.format("%d %s %s", errobj.code, errobj.message, data or err)
+    end
+    return nil, string.format("%d %s", errobj.code, errobj.message)
+  end
+  return result
+end
+
+local function request_sync_blocking(self, req, rid)
+  local ok, err = self.conn:send(req)
+  if not ok then
+    return nil, err
+  end
+  local result, errobj
+  self.pending_requests[rid] = function(res, err)
+    result = res
+    errobj = err
+  end
+  repeat
+    local msg, err = self.conn:recv()
+    if msg then
+      local ok, err = handle_message(self, msg)
+      if not ok then
+        return nil, err
+      end
+    elseif err ~= "timeout" then
+      return nil, err
+    end
+  until result or errobj
+  return return_result(result, errobj)
+end
+
+local function request_sync_nonblocking(self, req, rid, timeout)
+  local sema, err = ngx_semaphore.new()
+  if not sema then
+    return nil, err
+  end
+  local ok, err = self.conn:send(req)
+  if not ok then
+    return nil, err
+  end
+  local result, errobj
+  self.pending_requests[rid] = function(res, err)
+    result = res
+    errobj = err
+    sema:post()
+  end
+  local ok, err = sema:wait(tonumber(timeout) or 10)
+  if not ok then
+    return nil, err
+  end
+  return return_result(result, errobj)
+end
+
 local _M = {
   _NAME = "resty.mcp.session",
   _VERSION = mcp.version.module
@@ -41,65 +168,8 @@ function _M.new(conn, name, mt)
 end
 
 function _M.initialize(self, methods)
-  self.main_loop = ngx_thread_spawn(function()
-    local running_tasks = self.bg_tasks and {} or nil
-    while true do
-      local msg, err = self.conn:recv()
-      if msg then
-        local function handle_message()
-          local reply = mcp.rpc.handle(msg, methods, function(rid, result, errobj)
-            local cb = self.pending_requests[rid]
-            if not cb then
-              ngx_log(ngx.ERR, "response: request id mismatch")
-              return
-            end
-            self.pending_requests[rid] = nil
-            cb(result, errobj)
-          end)
-          if reply then
-            local ok, err = self.conn:send(reply)
-            if not ok then
-              ngx_log(ngx.ERR, "transport: ", err)
-              return nil, err
-            end
-          end
-          return true
-        end
-        if running_tasks then
-          local tid = mcp.utils.generate_id()
-          running_tasks[tid] = ngx_thread_spawn(function()
-            self.bg_tasks.count = self.bg_tasks.count + 1
-            handle_message()
-            self.bg_tasks.count = self.bg_tasks.count - 1
-            running_tasks[tid] = nil
-            if self.bg_tasks.count < 1 and self.bg_tasks.waiting > 0 then
-              self.bg_tasks.sema:post(self.bg_tasks.waiting)
-            end
-          end)
-        else
-          local ok, err = handle_message()
-          if not ok then
-            break
-          end
-        end
-      elseif err ~= "timeout" then
-        break
-      end
-    end
-    if running_tasks then
-      local tasks = {}
-      for k, v in pairs(running_tasks) do
-        table.insert(tasks, v)
-      end
-      if #tasks > 0 then
-        local ok, err = ngx_thread_wait(unpack(tasks))
-        if not ok then
-          ngx_log(ngx.ERR, "ngx thread: ", err)
-        end
-      end
-    end
-    self.main_loop = nil
-  end)
+  self.methods = methods
+  self.main_loop = ngx_thread_spawn(main_loop, self)
 end
 
 function _M.shutdown(self, kill)
@@ -128,47 +198,16 @@ function _M.send_request(self, name, args, cb_or_to)
   if type(name) ~= "string" or type(args) ~= "table" then
     error("invalid request format")
   end
-  local msg, rid, err = mcp.protocol.request[name](unpack(args))
-  if not msg then
+  local req, rid, err = mcp.protocol.request[name](unpack(args))
+  if not req then
     return nil, err
   end
   if cb_or_to and not tonumber(cb_or_to) then
-    local ok, err = self.conn:send(msg)
-    if not ok then
-      return nil, err
-    end
-    self.pending_requests[rid] = cb_or_to
-    return true
+    return request_async(self, req, rid, cb_or_to)
+  elseif self.conn.blocking_io then
+    return request_sync_blocking(self, req, rid)
   else
-    if self.conn.blocking_io then
-      error("blocking IO transport MUST use async mode")
-    end
-    local sema, err = ngx_semaphore.new()
-    if not sema then
-      return nil, err
-    end
-    local ok, err = self.conn:send(msg)
-    if not ok then
-      return nil, err
-    end
-    local result, errobj
-    self.pending_requests[rid] = function(r, e)
-      result = r
-      errobj = e
-      sema:post()
-    end
-    local ok, err = sema:wait(tonumber(cb_or_to) or 10)
-    if not ok then
-      return nil, err
-    end
-    if errobj then
-      if errobj.data then
-        local data, err = cjson.encode(errobj.data)
-        return nil, string.format("%d %s %s", errobj.code, errobj.message, data or err)
-      end
-      return nil, string.format("%d %s", errobj.code, errobj.message)
-    end
-    return result
+    return request_sync_nonblocking(self, req, rid, cb_or_to)
   end
 end
 
