9018418a2cca6a35a76ecd1c0a6589a2bba19cb6
RangerUFO <ufownl@gmail.com>
2025-06-30 00:24:14 +0800

    Bump the version of protocol and module

--- lib/resty/mcp/version.lua
+++ lib/resty/mcp/version.lua
@@ -1,7 +1,8 @@
 return {
-  module = "1.0",
-  protocol = "2025-03-26",
+  module = "2.0",
+  protocol = "2025-06-18",
   compatible_protocols = {
+    ["2025-03-26"] = true,
     ["2024-11-05"] = true
   }
 }
