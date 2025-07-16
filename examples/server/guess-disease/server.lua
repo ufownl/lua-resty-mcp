3c3a3695f8b3c4363c7f02b8b52e522e2c2ff73d
RangerUFO <ufownl@gmail.com>
2025-07-17 01:07:13 +0800

    Tweak guess-disease example

--- examples/server/guess-disease/server.lua
+++ examples/server/guess-disease/server.lua
@@ -3,7 +3,7 @@ local _M = {
   _VERSION = "1.0"
 }
 
-local server_instruction = "You are a host of the disease guessing game. Please reply in the same language as the request."
+local server_instruction = "You are the control terminal of the disease guessing game. You MUST reply with the result of the tool call in the same language as the user query."
 local assistant_prompt = "You are a helpful assistant who is proficient in medical knowledge."
 local function patient_prompt(self)
   return string.format("You are participating in a disease guessing game. You play the role of a patient in this game. The disease you suffer from may have the following symptoms:\n\n%s\n\nPlease note that the character you are playing is very lacking in medical knowledge and cannot accurately and fluently describe your condition to the doctor.", self.symptoms)
@@ -54,7 +54,7 @@ local function game_state(self, mcp, server)
             text = string.format("My name is %s and I am participating in a round of the disease guessing game. My guess is %s and the answer is %s. Please judge whether I guessed correctly.", self.name, args.disease, self.answer)
           }
         }
-      }, 2048, {
+      }, 4096, {
         systemPrompt = assistant_prompt,
         includeContext = "none",
         temperature = 0.2
@@ -118,7 +118,7 @@ function _MT.__index.initialize(self, mcp, server)
           text = string.format("My name is %s and the current date and time is %s. I am participating in a round of the disease guessing game. Please give me a list of real-life disease names (one disease name per line), I will select one of them as the answer to this round. Note that you should reply with only the names of these diseases, without description, explanation, or other content.", self.name, os.date("%c", ngx.now()))
         }
       }
-    }, 2048, {
+    }, 4096, {
       systemPrompt = assistant_prompt,
       includeContext = "none",
       temperature = 1.2
@@ -151,7 +151,7 @@ function _MT.__index.initialize(self, mcp, server)
           text = string.format("Please list all possible symptoms of %s (one symptom per line). Note that you should reply with only a list of the disease's symptoms, without any explanation, description, or other content, especially without the name or keywords of the disease.", self.answer)
         }
       }
-    }, 2048, {
+    }, 4096, {
       systemPrompt = assistant_prompt,
       includeContext = "none",
       temperature = 0.4
